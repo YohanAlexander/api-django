4029933dc5947625560af73e5b82c5c8616bc7ca
Yohan Alexander <yohanfranca@gmail.com>
2020-07-27 19:28:50 -0300

    🐛 fix url

--- crud/settings.py
+++ crud/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_on_heroku
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG') == True
 
-ALLOWED_HOSTS = ['https://api-ma9.herokuapp.com/']
+ALLOWED_HOSTS = ['https://desafio-ma9.herokuapp.com/']
 
 
 # Application definition
@@ -131,5 +131,5 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfile')
 
-django_on_heroku.settings(locals())
+django_heroku.settings(locals())
 
