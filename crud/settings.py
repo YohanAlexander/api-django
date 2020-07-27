d2ca09edce95d86c7fb59de4a2c1631386740ded
Yohan Alexander <yohanfranca@gmail.com>
2020-07-27 19:45:28 -0300

    🐛 revert before heroku

--- crud/settings.py
+++ crud/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -21,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'l6k^z)b19rlnbl_+e&=4f36r*nn$7--yppuid%6k6qht57+*q2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG') == True
+DEBUG = True
 
-ALLOWED_HOSTS = ['https://desafio-ma9.herokuapp.com/']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -128,8 +127,3 @@ PHONENUMBER_DEFAULT_REGION = 'BR'
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfile')
-
-django_heroku.settings(locals())
-
