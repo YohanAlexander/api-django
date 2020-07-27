57937bd2bffc5ff181c59f71336ea139af83d924
Yohan Alexander <yohanfranca@gmail.com>
2020-07-27 20:37:35 -0300

    ✨ add nested urls on api

--- api/urls.py
+++ api/urls.py
@@ -3,14 +3,17 @@ from django.urls import include
 
 from api.views import ClienteViewSet
 from api.views import EnderecoViewSet
-from rest_framework import routers
 
-router = routers.DefaultRouter()
+from rest_framework_nested import routers
+
+router = routers.SimpleRouter()
 router.register(r'cliente', ClienteViewSet)
-router.register(r'endereco', EnderecoViewSet)
+cliente_router = routers.NestedSimpleRouter(router, r'cliente', lookup='cliente')
+cliente_router.register(r'endereco', EnderecoViewSet)
 
 urlpatterns = [
     path('api/', include(router.urls)),
+    path('api/', include(cliente_router.urls)),
     path('api-auth/', include('rest_framework.urls'))
 ]
 
